78693d51ab954751a4a8ac30c9f7eb4c004af342
Nathan <nathnnovier@gmail.com>
2025-11-05 11:25:49 +0100

    v0.2.4 - Remove useless files

--- fonctionus/extractus.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-import pandas as pd
-from tqdm import tqdm
-from datetime import datetime
-
-ERROR_USER = ["Direct Message with Unknown Participant", "None", "Unknown channel"]
